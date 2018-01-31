dfc6a456790bba03e6af1f35d71b16aba019329b
fraser1234 <fraseralexanderblack@gmail.com>
2018-01-31 13:38:10 +0000

    list populating

--- public/index.js
+++ public/index.js
@@ -1,5 +1,3 @@
-
-
 var makeRequest = function(url, callback){
   var request = new XMLHttpRequest();
   request.open('GET', url);
@@ -7,30 +5,34 @@ var makeRequest = function(url, callback){
   request.send();
 }
 
-var populateList = function(countries){
-  var ul = document.querySelector('#country-list');
+var populateDropDown = function(countries){
+  var selectOption = document.querySelector('#country-list');
   countries.forEach(function(country){
-    var li = document.createElement('li');
-    li.innerText = country.name;
-    ul.appendChild(li);
+    var countryScript = document.createElement('option');
+    countryScript.text = country.name;
+    selectOption.appendChild(countryScript);
   });
 }
 
 var requestComplete = function(){
   // console.log('working');
-  if(this.status !== 200)
-    return;
+  if(this.status !== 200) return;
   var jsonString = this.responseText;
   var countries = JSON.parse(jsonString);
-  populateList(countries);
+  populateDropDown(countries);
 
 }
 
 var app = function(){
   var url = 'https://restcountries.eu/rest/v2';
-  var viewCountriesButton = document.getElementById('view-countries')
-  viewCountriesButton.addEventListener('click', function(){
-    makeRequest(url, requestComplete);
+  makeRequest(url, requestComplete);
+  // var viewCountriesButton = document.getElementById('view-countries')
+  // viewCountriesButton.addEventListener('change', function(){
+  //   makeRequest(url, requestComplete);
+  // })
+  var viewCountriesDropDown = document.getElementById('country-list');
+  viewCountriesDropDown.addEventListener('change', function () {
+
   })
 }
 
