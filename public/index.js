8501c602f6e0f9ff5ea2754cb71bbe598b853026
fraser1234 <fraseralexanderblack@gmail.com>
2018-01-31 14:27:57 +0000

    returning correct values

--- public/index.js
+++ public/index.js
@@ -7,9 +7,10 @@ var makeRequest = function(url, callback){
 
 var populateDropDown = function(countries){
   var selectOption = document.querySelector('#country-list');
-  countries.forEach(function(country){
+  countries.forEach(function(country, index){
     var countryScript = document.createElement('option');
     countryScript.text = country.name;
+    countryScript.value = index;
     selectOption.appendChild(countryScript);
   });
 }
@@ -20,20 +21,34 @@ var requestComplete = function(){
   var jsonString = this.responseText;
   var countries = JSON.parse(jsonString);
   populateDropDown(countries);
+  selectedCountry(countries);
 
+}
+
+var selectedCountry = function(countries){
+  var selectedResult = document.getElementById('country-info');
+  var viewCountriesDropDown = document.getElementById('country-list');
+  viewCountriesDropDown.addEventListener('change', function () {
+    selectedResult.innerHTML = "Country: " + countries[viewCountriesDropDown.value].name + "<br>" +
+    "Capital: " + countries[viewCountriesDropDown.value].capital + "<br>" +
+    "Population: " + countries[viewCountriesDropDown.value].population;
+
+
+
+  })
 }
 
 var app = function(){
   var url = 'https://restcountries.eu/rest/v2';
   makeRequest(url, requestComplete);
-  // var viewCountriesButton = document.getElementById('view-countries')
-  // viewCountriesButton.addEventListener('change', function(){
-  //   makeRequest(url, requestComplete);
+  // var viewCountriesDropDown = document.getElementById('country-list');
+  // viewCountriesDropDown.addEventListener('change', function () {
+  //   // console.log(this.options[this.selectedIndex].text);
+  //   selectedResult.innerHTML = "Country: " + this.countries.capital;
+  //   // selectedResult.innerHTML = "Population: " + this.options[this.selectedIndex].text;
+  //   // selectedResult.innerHTML = "Country: " + this.options[this.selectedIndex].text;
+  //   // selectedResult.innerHTML = "Country: " + this.options[this.selectedIndex].text;
   // })
-  var viewCountriesDropDown = document.getElementById('country-list');
-  viewCountriesDropDown.addEventListener('change', function () {
-
-  })
 }
 
 window.addEventListener('load', app);
